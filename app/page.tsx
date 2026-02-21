09b7a1f28d5e6585a9d137fabd0a97d0a06cfdab
Daniel <dancebroa@gmail.com>
2026-02-21 19:27:31 +0100

    chore: change background colors

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
           </p>
 
           <div className="mt-8 flex flex-wrap gap-3">
-            <Button asChild>
+            <Button variant="outline" asChild>
               <a
                 href="https://github.com/dancebroa-001"
                 target="_blank"
